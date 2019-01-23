e5ed739ed11b1fdde7cca6b0397b0b3041d53d02
Maushimo <Maushimo@users.noreply.github.com>
2019-01-23 18:49:12 +0000

    can now add projects

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-#projects = ['Personal Website', 'ReactJS game', 'Crazy Taxi Clone', 'VN Review', 'Read/Write', 'New EP']
-
 import random
 import csv
+
+def addProject():
+	project = raw_input("What would you like to add?\n")
+
+	with open('projects.csv', 'a') as csvFile:
+		csvFile.write(",\n" + project)
+
+	print("You have successfully added " + project)
 
 projects = []
 
@@ -13,14 +19,19 @@ with open('projects.csv') as csvFile:
 
 choice = input("PRODUCTIVITY WHEEL:\n [1] Spin the wheel\n [2] Add Items\n [3] Remove Items\n")
 
+# SPIN
 if choice == 1:
 	random.shuffle(projects)
 	garbageChars = "[',]" # string containing random characters that the thing prints for some reason
 	print("Work on " + str(projects[0]).translate(None, garbageChars))
+# ADD
 elif choice == 2:
-	print("Implement feature")
+	addProject()
+	#print("Implement feature")
+# DELETE
 elif choice == 3:
 	print("Implement feature")
+# PRINT (TEST)
 elif choice == 4:
 	for i in range(len(projects)):
 		print projects[i]
