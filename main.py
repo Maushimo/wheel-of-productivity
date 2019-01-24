7ba7b75b898e2477ee1cdc6da32f58fbe39ba7f4
Maushimo <Maushimo@users.noreply.github.com>
2019-01-24 17:47:14 +0000

    now checks if csv exists

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import csv
+import os
 
 def addProject():
 	project = raw_input("What would you like to add?\n")
@@ -9,29 +10,29 @@ def addProject():
 
 	print("You have successfully added " + project)
 
-projects = []
+if os.path.exists('./projects.csv'):
+	projects = []
+	with open('projects.csv') as csvFile:
+		csvReader = csv.reader(csvFile, delimiter=',')
+		for row in csvReader:
+			projects.append(row)
 
-with open('projects.csv') as csvFile:
-	csvReader = csv.reader(csvFile, delimiter=',')
-	for row in csvReader:
-		projects.append(row)
-
-
-choice = input("PRODUCTIVITY WHEEL:\n [1] Spin the wheel\n [2] Add Items\n [3] Remove Items\n")
-
-# SPIN
-if choice == 1:
-	random.shuffle(projects)
-	garbageChars = "[',]" # string containing random characters that the thing prints for some reason
-	print("Work on " + str(projects[0]).translate(None, garbageChars))
-# ADD
-elif choice == 2:
-	addProject()
-	#print("Implement feature")
-# DELETE
-elif choice == 3:
-	print("Implement feature")
-# PRINT (TEST)
-elif choice == 4:
-	for i in range(len(projects)):
-		print projects[i]
+	# Make the choice
+	choice = input("PRODUCTIVITY WHEEL:\n [1] Spin the wheel\n [2] Add Items\n [3] Remove Items\n")
+	# SPIN
+	if choice == 1:
+		random.shuffle(projects)
+		garbageChars = "[',]" # string containing random characters that the thing prints for some reason
+		print("Work on " + str(projects[0]).translate(None, garbageChars))
+	# ADD
+	elif choice == 2:
+		addProject()
+	# DELETE
+	elif choice == 3:
+		print("Implement feature")
+	# PRINT (TEST)
+	elif choice == 4:
+		for i in range(len(projects)):
+			print(projects[i])
+else:
+	print("Unable to locate 'projects.csv' file")
