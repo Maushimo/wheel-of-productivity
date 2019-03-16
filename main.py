406f93a274ca90084cda8554c899d29d5bde9a60
Maushimo <Maushimo@users.noreply.github.com>
2019-03-16 16:22:51 +0000

    can remove objects from list but adds random characters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import csv
 import os
 
+# FUNCTIONS
 def addProject():
 	project = raw_input("What would you like to add?\n")
 
@@ -10,6 +11,31 @@ def addProject():
 
 	print("You have successfully added " + project)
 
+def remProject(projects):
+	# empty string to put the list of projects in
+	projectList = ""
+	for i in range(len(projects)):
+		projectList += ("[" + str(i) + "]" + str(projects[i]) + "\n")
+
+	garbageChars = "'," # string containing random characters that the thing prints for some reason
+	#print("You can remove the following:\n\n" + projectList.translate(None, garbageChars) + "\n")
+	choice = input("Which project do you want to remove?\n" + projectList.translate(None, garbageChars) + "\n")
+	
+	# remove project
+	projects.pop(choice)
+	
+	# fill new list
+	for i in range(len(projects)):
+		projectList += ("[" + str(i) + "]" + str(projects[i]) + "\n")
+	print(projectList.translate(None, garbageChars))
+
+	with open('projects.csv', 'w+') as csvFile:
+		fileWriter = csv.writer(csvFile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+		for i in range(len(projects)):
+			fileWriter.writerow([projects[i]])
+
+
+# MAIN BIT
 if os.path.exists('./projects.csv'):
 	projects = []
 	with open('projects.csv') as csvFile:
@@ -29,10 +55,13 @@ if os.path.exists('./projects.csv'):
 		addProject()
 	# DELETE
 	elif choice == 3:
-		print("Implement feature")
+		remProject(projects)
+		#print("Implement feature")
 	# PRINT (TEST)
 	elif choice == 4:
 		for i in range(len(projects)):
 			print(projects[i])
+	else:
+		print("Invalid input")
 else:
 	print("Unable to locate 'projects.csv' file")
